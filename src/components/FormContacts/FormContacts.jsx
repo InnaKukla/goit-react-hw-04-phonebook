1e4aff4eb675ea1aa113d61846a6e744e63f0058
Inna Kukla <0969125133@ukr.net>
2023-03-07 20:08:42 +0200

    Create FormContacts.jsx

---
+++ src/components/FormContacts/FormContacts.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import { FormAddcontact } from './FormContacts.styled';
+
+export const FormContacts = ({ onSubmitHandler }) => {
+  return (
+    <>
+      <FormAddcontact onSubmit={onSubmitHandler}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          ></input>
+        </label>
+
+        <label>
+          Number
+          <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          ></input>
+        </label>
+        <button>Add contact</button>
+      </FormAddcontact>
+    </>
+  );
+};
+
+FormContacts.propTypes = {
+  onSubmitHandler: PropTypes.func.isRequired,
+};
